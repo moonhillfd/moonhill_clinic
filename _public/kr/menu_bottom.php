1e6abbf2d81a24eec1911c8027cc1d701d311b29
moonhillfood <mastercool@moonhillfood.com>
2020-04-23 16:10:34 +0900

    - 원장님 수정사항 적용 완료

---
+++ _public/kr/menu_bottom.php
@@ -0,0 +1,66 @@
+    <div class="clinic-box">
+        <p class="tit">해독 클리닉</p>
+        <p class="txt">독소로 오염된 몸은 만병의 근원이며, 해독은 만병 치료의 시작입니다.</p>
+        <duv class="btn-box">
+            <a href="/?folder=decode&page=decode01" class="btn">해독 클리닉 바로가기</a>
+        </duv>
+    </div>
+    <footer>
+        <div class="footer-top">
+          <div class="footer-wrap">
+              <div class="box info1">
+                <p class="tit">진료예약 문의</p>
+                <p class="txt"><span>전화예약</span>051-746-7711</p>
+                <p class="txt"><span>카톡문의</span>010-5815-7712</p>
+                <div class="btn-box">
+                    <a href="?folder=community&page=community04" class="btn">온라인 상담</a>
+                </div>
+              </div>
+              <div class="box info2">
+                <p class="tit">진료시간 안내</p>
+                <p class="txt"><span>월/화/수/금</span>오전 9:30 ~ 오후 6:00 </p>
+                <!--<p class="txt"><span>공휴일</span>오전 9:30 ~ 오후 6:00 </p>-->
+                <p class="txt"><span>토요일</span>오전 9:30 ~ 오후 3:00 </p>
+                <p class="txt"><span>목/일</span>휴진</p>
+                <p class="txt2">점심시간 오후 12:30 ~ 오후 2시</p>
+                <p class="txt2">※ 토요일은 점심시간 없이 진료합니다.</p>
+              </div>
+              <div class="box info3">
+                <p class="tit">오시는 길</p>
+                <p class="txt">부산 해운대구 달맞이길117번가길 149</p>
+                <div class="btn-box">
+                  <a href="/?folder=moon&page=moon06" class="btn">상세보기</a>
+                </div>
+              </div>
+          </div>
+        </div>
+        <div class="nav">
+            <ul>
+                <li><a href="/?folder=member&page=member05">이용약관</a></li>
+                <li><a href="/?folder=member&page=member06">개인정보취급방침</a></li>
+            </ul>
+        </div>
+        <div class="footer-bottom">
+            <div class="footer-wrap">
+              <a href="./" class="footer-logo"><img src="/_public/images/common/footer_logo.png" alt=""></a>
+              <div class="footer-info">
+                  <p><span>상호 : 달맞이한의원</span><span>대표자 : 허정구</span><span>사업자등록번호 : 617-91-20914</span></p>
+                  <p>주소 : 부산 해운대구 달맞이길117번가길 149 달맞이한의원</p>
+                  <p><span>전화 : 051-746-7711</span><span>팩스 : 051-747-1077</span></p>
+              </div>
+              <div class="sns">
+                <span><a href="http://blog.naver.com/PostList.nhn?blogId=antitumor454&skinType=&skinId=&from=menu&userSelectMenu=true" target="_blank"><img src="/_public/images/common/footer_sns_icon01.png" alt=""></a></span>
+                <span><a href="https://pf.kakao.com/_hiByl/42700638" target="_blank"><img src="/_public/images/common/footer_sns_icon02.png" alt=""></a></span>
+                <p class="copyright">Copyright by Moonhill 2019.All rights reserved.</p>
+              </div>
+            </div>
+        </div>
+    </footer>
+    <div id="quick-menu">
+        <a href="/?folder=bbs&page=list&board_id=counsel">온라인상담</a>
+        <a href="?folder=community&page=community04">진료예약</a>
+        <a href="https://pf.kakao.com/_hiByl/42700638" target="_blank">카카오상담</a>
+        <a href="http://blog.naver.com/PostList.nhn?blogId=antitumor454&skinType=&skinId=&from=menu&userSelectMenu=true" target="_blank">네이버블로그</a>
+        <a href="/?folder=moon&page=moon06">오시는길</a>
+        <a href="#" class="top-scroll">TOP</a>
+    </div>
---
+++ _public/kr/menu_top.php
@@ -0,0 +1,142 @@
+<?
+
+switch($folder){
+	case"moon":
+		$print_depth1_displays[0] = 'style="display:block;"';
+	break;
+
+	case"atopy":
+		$print_depth1_displays[1] = 'style="display:block;"';
+	break;
+
+	case"decode":
+		$print_depth1_displays[2] = 'style="display:block;"';
+	break;
+
+	case"cancer":
+		$print_depth1_displays[3] = 'style="display:block;"';
+	break;
+
+	case"headache":
+		$print_depth1_displays[4] = 'style="display:block;"';
+	break;
+
+	case"etc":
+		$print_depth1_displays[5] = 'style="display:block;"';
+	break;
+
+	case"medicine":
+		$print_depth1_displays[6] = 'style="display:block;"';
+	break;
+
+	case"bbs":
+		$print_depth1_displays[7] = 'style="display:block;"';
+	break;
+
+	case"community":
+		$print_depth1_displays[7] = 'style="display:block;"';
+	break;
+
+
+}
+?>
+<div id="wrap">
+    <header>
+        <div class="header-top">
+            <div class="header-wrap">
+                <div class="header-util">
+                    <a href="./">HOME</a>                    
+                    <!-- 로그인 로그아웃 문구 변경 해주세요. -->
+                    <? if(isLogin() !== true){ ?>
+					<a href="/?folder=member&page=join">회원가입</a>
+					<a href="/?folder=member&page=login">로그인</a>
+					<?} else {?>
+					<a href="/?folder=member&page=proc&mode=logout">로그아웃</a>
+					<?}?>
+                </div>
+                <a href="./" class="header-logo"><img src="/_public/images/common/logo.png" alt=""></a>
+                <a href="#" class="nav-icon">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </a>
+                <div class="gnb">
+                    <ul>
+                        <li>
+                          <a href="/?folder=moon&page=moon01">달맞이 한의원</a>
+                          <div class="sub-menu" <?=$print_depth1_displays[0]?>>
+                            <a href="/?folder=moon&page=moon01" class="point">달맞이한의원의 특별함</a>
+                            <a href="/?folder=moon&page=moon02">원장 소개</a>
+                            <a href="/?folder=moon&page=moon03">약재 관리</a>
+                            <a href="/?folder=moon&page=moon04">진료 안내</a>
+                            <a href="/?folder=moon&page=moon05">시설 둘러보기</a>
+                            <a href="/?folder=moon&page=moon06">오시는 길</a>
+                          </div>
+                        </li>
+                        <li>
+                            <a href="/?folder=atopy&page=atopy01">아토피&middot;건선 치료</a>
+                            <div class="sub-menu" <?=$print_depth1_displays[1]?>>
+                                <a href="/?folder=atopy&page=atopy01">아토피건선치료</a>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="/?folder=decode&page=decode01">해독클리닉</a>
+                            <div class="sub-menu" <?=$print_depth1_displays[2]?>>
+                                <a href="/?folder=decode&page=decode01">해독의 이해</a>
+                                <a href="/?folder=decode&page=decode02">금진옥액</a>
+                                <a href="/?folder=decode&page=decode03">간해독</a>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="/?folder=cancer&page=cancer01">암클리닉</a>
+                            <div class="sub-menu" <?=$print_depth1_displays[3]?>>
+                                <a href="/?folder=cancer&page=cancer01">암의 이해</a>
+                                <a href="/?folder=cancer&page=cancer02">달맞이 암 특화치료</a>
+                                <a href="/?folder=cancer&page=cancer03">달맞이 항암설계치료</a>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="/?folder=headache&page=headache01">두통&middot;편두통</a>
+                            <div class="sub-menu" <?=$print_depth1_displays[4]?>>
+                                <a href="/?folder=headache&page=headache01">두통&middot;편두통 치료</a>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="/?folder=etc&page=etc01">기타치료</a>
+                            <div class="sub-menu" <?=$print_depth1_displays[5]?>>
+                                <a href="/?folder=etc&page=etc01">치매&middot;건망증</a>
+                                <a href="/?folder=etc&page=etc04">수족냉증</a>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="/?folder=medicine&page=medicine01">달맞이환약</a>
+                            <div class="sub-menu" <?=$print_depth1_displays[6]?>>
+                                <a href="/?folder=medicine&page=medicine01">공진단</a>
+                                <a href="/?folder=medicine&page=medicine02">생정대력환</a>
+                                <a href="/?folder=medicine&page=medicine03">운부환</a>
+                                <a href="/?folder=medicine&page=medicine04">온백원</a>
+                                <a href="/?folder=medicine&page=medicine05">자궁단</a>
+                                <a href="/?folder=medicine&page=medicine06">복명환</a>
+                                <a href="/?folder=medicine&page=medicine07">대풍자환</a>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="/?folder=bbs&page=list&board_id=notice">커뮤니티</a>
+                            <div class="sub-menu" <?=$print_depth1_displays[7]?>>
+                                <a href="/?folder=bbs&page=list&board_id=notice">공지사항</a>
+                                <a href="/?folder=bbs&page=list&board_id=news">언론보도</a>
+                                <a href="/?folder=bbs&page=list&board_id=counsel">온라인 상담</a>
+                                <a href="/?folder=community&page=community04">진료예약</a>
+                                <a href="/?folder=bbs&page=list&board_id=reward">치료사례</a>
+                                <a href="/?folder=bbs&page=list&board_id=after">치료후기</a>
+                                <a href="/?folder=community&page=community07">책자신청</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="header-bottom"></div>
+    </header>
+
